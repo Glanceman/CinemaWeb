fc450a9039a04c441be5e67d706babebfaf0a336
Glanceman <72264741+Glanceman@users.noreply.github.com>
2022-04-18 20:33:36 +0800

    Add files via upload
    init

---
+++ JS/movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Cassblanca",
+            cast:"Humphrey Bogart, Ingrid Bergmen",
+            director:"Michael Curtiz",
+            duration: 130
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"wildlife.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Wildlife",
+            cast:" Hailee Steinfeld， John Cena， Jorge Lendeborg， Jr. John Ortiz， JasonDrucker，Pamela Adlon",
+            director:"Forest King",
+            duration: 121
+        },
+        {
+            id:3,
+            type:"upcoming",
+            thumbnail:"nature.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Nature",
+            cast:"Parrot, birds",
+            director:"Mother Nature",
+            duration: 131
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"cakemaking.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/cakemaking-s.mp4",
+            name:"Cakemaking",
+            cast:"Tom Holland, Zendaya, Jacob Batalon",
+            director:"Jon Watts",
+            duration: 176
+        },
+        {
+            id:5,
+            type:"now",
+            thumbnail:"chocolate.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name:"Chocolate",
+            cast:"Angelina Jolie, Gemma Chan, Richard Madden, Kumail Nanjiani, Lauren Ridloff,Brian Tyree Henry, Salma Hayek, Lia McHugh, Don Lee and Kit Harrington",
+            director:"Chloé Zhao",
+            duration: 131
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"wonders.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name:"Wonders",
+            cast:"Justin Lin",
+            director:"Vin Diesel，Michelle Rodriguez，Tyrese Gibson， Chris Ludacris Bridges",
+            duration: 143
+        },
+    ]
+}
